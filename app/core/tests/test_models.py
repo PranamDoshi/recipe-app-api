dfc0a80632273b4f404ef199ef1b4a0b41b6ced7
PranamDoshi <pranam.rpdoshi@gmail.com>
2022-07-25 01:14:31 +0530

    Added Tags Feature to reciepes

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,6 +9,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(**params):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(**params)
+
+
 class ModelTests(TestCase):
     """Test Models."""
 
@@ -67,3 +72,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag_successful(self):
+        """Test Creating a tag is successful."""
+        user = create_user(email='test@example.com', password='test123')
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
