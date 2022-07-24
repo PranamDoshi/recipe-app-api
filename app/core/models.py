8483c8058cfdc7b00f19d70d71b3fa996d83b1fb
PranamDoshi <pranam.rpdoshi@gmail.com>
2022-07-24 17:30:08 +0530

    Added Recipe App and APIs

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models.
 """
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -41,3 +42,22 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe Object."""
+    # Foreign Key is used to set-up a relationship between
+    # User model and recipe model.
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title= models.CharField(max_length=255)
+    # Text Field can store multiple lines and more characters.
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
---
+++ app/recipe/urls.py
@@ -0,0 +1,23 @@
+"""
+URL Mappings for the recipe app.
+"""
+from django.urls import (
+    path,
+    include,
+)
+
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+
+# Router is used to create urls for all the different available options 
+# for the given viewset.
+router = DefaultRouter()
+router.register('recipes', views.RecipeViewSet)
+
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
