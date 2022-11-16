1044b133a976bb2c516d9b1aa19e16dca61011b1
PranamDoshi <pranam.rpdoshi@gmail.com>
2022-11-16 21:04:14 +0530

    Added proxy and uwsgi server for server deployment

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ set -e # Terminates the sript if any commands fail
 
 python manage.py wait_for_db
 # Collects and stores all the static files in the static files directory
-python manage.py collectstatis --noinput
+python manage.py collectstatic --noinput
 python manage.py migrate
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
